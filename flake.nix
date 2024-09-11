a2812bb7541caf4ec0dab1fa8177c88d8bc193a8
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-11 09:43:32 +0200

    ci: add git-hooks checks

--- flake.nix
+++ flake.nix
@@ -9,12 +9,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks = {
+      # TODO: https://github.com/cachix/git-hooks.nix/pull/396
+      # url = "github:cachix/git-hooks.nix";
+      url = "github:mrcjkb/git-hooks.nix?ref=clippy";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
     flake-parts,
+    git-hooks,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -24,33 +31,56 @@
         "aarch64-linux"
         "aarch64-darwin"
       ];
-      perSystem = {
-        config,
-        system,
-        ...
-      }: let
+      perSystem = {system, ...}: let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
             self.overlays.default
           ];
         };
+        git-hooks-check = git-hooks.lib.${system}.run {
+          src = self;
+          hooks = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
+            clippy = {
+              enable = true;
+              settings = {
+                denyWarnings = true;
+                allFeatures = true;
+              };
+              extraPackages = with pkgs.vimcats; buildInputs ++ nativeBuildInputs;
+            };
+            cargo-check.enable = true;
+          };
+          settings = {
+            rust.check.cargoDeps = pkgs.rustPlatform.importCargoLock {
+              lockFile = ./Cargo.lock;
+            };
+          };
+        };
       in {
         devShells.default = pkgs.mkShell {
           name = "vimcats devShell";
-          buildInputs = with pkgs;
-          with pkgs.rustPlatform; [
-            cargo
-            rustc
-            rustfmt
-            rust-analyzer
-            alejandra
-          ];
+          buildInputs =
+            pkgs.vimcats.buildInputs
+            ++ pkgs.vimcats.nativeBuildInputs
+            ++ self.checks.${system}.git-hooks-check.enabledPackages
+            ++ (with pkgs; [
+              rust-analyzer
+            ]);
+          inherit (git-hooks-check) shellHook;
+          doCheck = false;
         };
 
         packages = rec {
           default = vimcats;
           inherit (pkgs) vimcats;
+        };
+
+        checks = rec {
+          default = git-hooks-check;
+          inherit git-hooks-check;
         };
       };
       flake = {
